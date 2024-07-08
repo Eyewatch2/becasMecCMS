39f1a0d7a1edaf2851072830215be08c97e3f7a1
PolCarva <pablocarvalhogimenez@gmail.com>
2024-07-08 11:22:35 -0300

    Update server for railway

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express from "express";
-import payload from "payload";
+import express from 'express'
+import payload from 'payload'
 
-require("dotenv").config();
-const app = express();
+require('dotenv').config()
+const app = express()
+
 
 // Redirect root to Admin panel
-app.get("/", (_, res) => {
-  res.redirect("/admin");
-});
+app.get('/', (_, res) => {
+  res.redirect('/admin')
+})
 
 const start = async () => {
   // Initialize Payload
@@ -15,15 +16,19 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
     },
-  });
+  })
 
   // Add your own express routes here
 
-  app.listen(3000, "0.0.0.0", () => {
-    payload.logger.info(`Server running on port 3000`);
-  });
-};
+  
+  const port = parseInt(process.env.PORT || '3000', 10);
 
-start();
+  // Listen on `port` and 0.0.0.0
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`)
+  });
+}
+
+start()
